d3dd61d5d5e04a5e2807b52f99101f21e1b0c24c
baloerrr <99787895+baloerrr@users.noreply.github.com>
2022-12-27 21:11:52 +0700

    first commit

---
+++ indonesia.php
@@ -0,0 +1,38 @@
+<?php
+
+//menu utama
+define ("language", 'Bahasa');
+define ("home", 'Beranda');
+define ("services", 'Pelayanan');
+define ("gallery", 'Galleri');
+define ("aboutus", 'Tentang Kami');
+define ("contactus", 'Kontak');
+
+//tombol
+define ("moredetail", "Cek Katalog Produk");
+define ("buttoncontinue", "lihat Galeri Kami");
+define ("buttonsendemail", "Kirim Pesan");
+
+//judul
+define ("labelgallery", "Galeri");
+define ("labelgallerydescription", "Galeri Foto Produk dan Pelayanan Kami");
+define ("labelservices", "Pelayanan");
+define ("labelaboutus", "Sekilas Tentang Kami");
+define ("labelcontactus", "Kontak Kami");
+
+//form email
+define ("placename", "Nama Anda...");
+define ("placesubject", "Subjek...");
+define ("placeemail", "Alamat Email Anda...");
+define ("placemessage", "Isi Pesan...");
+define ("buttonsendmail", "Kirim Pesan");
+
+//footer
+define ("labellocation", "Lokasi");
+define ("labelinfo", "Informasi Lengkap");
+define ("labelheadoffice", "Alamat Lengkap");
+define ("labelphone", "Telepon");
+define ("labelemail", "Email");
+define ("labelsosmed", "Media Sosial");
+
+?>
